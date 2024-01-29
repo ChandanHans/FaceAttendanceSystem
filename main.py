99e607a16c109d28ef70e5cf697f81be6946031c
Your Name <chandanhans2003@gmail.com>
2024-01-30 02:57:54 +0530

    new

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import json
+from tkinter import ttk
 from customtkinter import *
 from PIL import Image
 import mysql.connector
+import tkinter as tk
+import cv2
+import face_recognition
 
 
 class Database:
@@ -42,6 +46,7 @@ class Database:
         """Close the database connection."""
         if self.conn:
             self.conn.close()
+
 
 class MyApp(CTk):
     def __init__(self):
@@ -94,7 +99,8 @@ class MyApp(CTk):
             if widget != self.title_label:
                 widget.pack_forget()
         frame_to_show.pack(fill="both", expand=True)
-        
+
+
 class HomeFrame(CTkFrame):
     def __init__(self, parent: MyApp, **kwargs):
         super().__init__(parent, **kwargs)
@@ -145,14 +151,37 @@ class HomeFrame(CTkFrame):
         )
         self.button_attendance.place(relx=0.5, rely=0.5, relwidth=0.3, anchor="center")
 
+        # Frame as a container for label and combobox
+        self.camera_selection_container = CTkLabel(self, text="", width=260)
+        self.camera_selection_container.place(relx=0.5, rely=0.65, anchor="center")
+
+        # ComboBox for camera selection inside the container
+        self.camera_selection = CTkComboBox(
+            self.camera_selection_container,
+            values=[str(i) for i in range(0, 20)],
+            state="readonly",
+            width=60,
+            command=self.save_choice,
+        )
+        self.camera_selection.place(y=0, x=0)
+        self.camera_selection.set("0")
+
+        # Label for camera selection inside the container
+        self.camera_selection_label = CTkLabel(
+            self.camera_selection_container,
+            text="Select Camera",
+            font=("Arial", 20, "bold"),
+        )
+        self.camera_selection_label.place(x=80, y=0)
+
         # Checkboxes for options
         self.checkbox_show_camera = CTkCheckBox(
             self,
             command=lambda: self.save_choice(),
-            text="   Show camera   ",
+            text="   Show Camera   ",
             font=("Arial", 20, "bold"),
         )
-        self.checkbox_show_camera.place(relx=0.5, rely=0.65, anchor="center")
+        self.checkbox_show_camera.place(relx=0.5, rely=0.75, anchor="center")
 
         self.checkbox_auto_start = CTkCheckBox(
             self,
@@ -160,14 +189,18 @@ class HomeFrame(CTkFrame):
             text="   Auto Start         ",
             font=("Arial", 20, "bold"),
         )
-        self.checkbox_auto_start.place(relx=0.5, rely=0.75, anchor="center")
+        self.checkbox_auto_start.place(relx=0.5, rely=0.85, anchor="center")
 
         # Load previous choices if any
         self.load_previous_choices()
 
     # Utility Functions
-    def save_choice(self):
-        choice = [self.checkbox_show_camera.get(), self.checkbox_auto_start.get()]
+    def save_choice(self, *args):
+        choice = [
+            self.camera_selection.get(),
+            self.checkbox_show_camera.get(),
+            self.checkbox_auto_start.get(),
+        ]
         with open("choice.json", "w") as f:
             json.dump(choice, f)
 
@@ -177,43 +210,54 @@ class HomeFrame(CTkFrame):
                 choice = json.load(f)
         except (FileNotFoundError, json.JSONDecodeError):
             choice = [
+                "0",
                 0,
                 0,
             ]  # Default values if the file doesn't exist or contains errors
+            self.save_choice()
         return choice
 
     def load_previous_choices(self):
         choices = self.get_choice()
-        if choices[0]:
-            self.checkbox_show_camera.select()
+        self.camera_selection.set(choices[0])
         if choices[1]:
+            self.checkbox_show_camera.select()
+        if choices[2]:
             self.checkbox_auto_start.select()
+
 
 class AddStudentFrame(CTkFrame):
     def __init__(self, parent, db: Database, **kwargs):
         super().__init__(parent, **kwargs)
         self.db = db
+        self.entry1 = StringVar()
+        self.entry2 = StringVar()
         self.configure(border_width=4, border_color="#5665EF")
         self.create_widgets()
 
     def create_widgets(self):
-        self.ask_data_frame = CTkFrame(self, border_width=4, border_color="#5665EF",corner_radius=0)
+        self.ask_data_frame = CTkFrame(
+            self, border_width=4, border_color="#5665EF", corner_radius=0
+        )
         self.ask_data_frame.place(relx=0, rely=0, relwidth=0.4, relheight=0.5)
 
         self.name_label = CTkLabel(self.ask_data_frame, text="Name:")
         self.name_label.place(relx=0.1, rely=0.1)
-        self.name_entry = CTkEntry(self.ask_data_frame)
+        self.name_entry = CTkEntry(self.ask_data_frame, textvariable=self.entry1)
         self.name_entry.place(relx=0.4, rely=0.1, relwidth=0.5)
 
         self.roll_label = CTkLabel(self.ask_data_frame, text="Roll Number:")
         self.roll_label.place(relx=0.1, rely=0.25)
-        self.roll_entry = CTkEntry(self.ask_data_frame)
+        self.roll_entry = CTkEntry(self.ask_data_frame, textvariable=self.entry2)
         self.roll_entry.place(relx=0.4, rely=0.25, relwidth=0.5)
 
         self.course_label = CTkLabel(self.ask_data_frame, text="Course:")
         self.course_label.place(relx=0.1, rely=0.4)
         self.course_entry = CTkComboBox(
-            self.ask_data_frame, values=["BSC", "BCA", "BBA"], state="readonly"
+            self.ask_data_frame,
+            command=self.on_entry_update,
+            values=["BSC", "BCA", "BBA"],
+            state="readonly",
         )
         self.course_entry.place(relx=0.4, rely=0.4, relwidth=0.5)
 
@@ -221,37 +265,45 @@ class AddStudentFrame(CTkFrame):
         self.sem_label.place(relx=0.1, rely=0.55)
         self.sem_entry = CTkComboBox(
             self.ask_data_frame,
+            command=self.on_entry_update,
             values=["1st", "2nd", "3rd", "4th", "5th", "6th"],
             state="readonly",
         )
         self.sem_entry.place(relx=0.4, rely=0.55, relwidth=0.5)
 
         self.submit_button = CTkButton(
-            self.ask_data_frame, text="Add Student", command=lambda: self.add_student()
+            self.ask_data_frame, text="Add Student", command=self.add_student
         )
         self.submit_button.place(relx=0.5, rely=0.87, anchor="center")
 
         #  camera_frame
-        self.show_camera_frame = CTkFrame(self, border_width=4, border_color="#5665EF",corner_radius=0)
+        self.show_camera_frame = ShowCameraFrame(self, 1)
         self.show_camera_frame.place(relx=0, rely=0.5, relwidth=0.4, relheight=0.5)
 
-        self.show_data_frame = ShowDataFrame(self, self.db,corner_radius=0)
-        self.show_data_frame.place(relx=0.4, rely=0, relwidth=0.6, relheight=1)
+        self.student_table_frame = StudentTableFrame(self, self.db)
+        self.student_table_frame.place(relx=0.4, rely=0, relwidth=0.6, relheight=1)
 
         self.ok_logo = CTkImage(Image.open("src/ok.png"), size=(30, 30))
         self.cancel_logo = CTkImage(Image.open("src/cancel.png"), size=(30, 30))
         self.status_label = CTkLabel(
             self.ask_data_frame, text="", fg_color="transparent"
         )
+        self.entry1.trace_add("write", self.on_entry_update)
+        self.entry2.trace_add("write", self.on_entry_update)
+
+    def on_entry_update(self, *args):
+        self.status_label.place_forget()
 
     def add_student(self):
-        roll = self.roll_entry.get()
-        name = self.name_entry.get()
+        roll = self.roll_entry.get().strip()
+        name = self.name_entry.get().strip()
         cource = self.course_entry.get()
         sem = self.sem_entry.get()
         if roll == "" or name == "" or cource == "" or sem == "":
             self.status_label.configure(image=self.cancel_logo)
         else:
+            self.submit_button.configure(state=DISABLED)
+            self.show_camera_frame.start_camera()
             success = self.db.execute_query(
                 """INSERT INTO Students (Roll, SName, Course, Sem)
                 VALUES (%s, %s, %s, %s)
@@ -266,11 +318,127 @@ class AddStudentFrame(CTkFrame):
                 self.status_label.configure(image=self.cancel_logo)
         self.status_label.place(relx=0.5, rely=0.75, anchor="center")
 
-class ShowDataFrame(CTkFrame):
+    def save_images(self, roll: str):
+        # Corrected indentation
+        student_folder_path = "Student_Face/" + str(roll.upper())
+        if not os.path.exists(student_folder_path):
+            os.makedirs(student_folder_path)
+
+
+class ShowCameraFrame(CTkFrame):
+    def __init__(self, parent: AddStudentFrame, camera_index, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.parent = parent
+        self.camera_index = camera_index
+        self.cap = None
+        self.running = False
+        self.frame_count = 0
+        self.start = False
+        self.configure(border_width=4, border_color="#5665EF", corner_radius=0)
+        self.image_label = CTkLabel(self, text="")
+
+        self.button_frame = CTkLabel(self.image_label, text="", height=30)
+        self.button_frame.place(relwidth=1)
+        # Start Button
+        self.start_button = CTkButton(
+            self.button_frame, text="Start", command=self.save_face, width=60
+        )
+        self.start_button.place(rely=0.5, relx=0.3, anchor=CENTER)
+        # Cancel Button
+        self.cancel_button = CTkButton(
+            self.button_frame, text="Cancel", command=self.stop_camera, width=60
+        )
+        self.cancel_button.place(rely=0.5, relx=0.7, anchor=CENTER)
+
+    def start_camera(self):
+        self.student_folder_path = "Student_Face/" + str(
+            self.parent.roll_entry.get().strip().upper()
+        )
+        if not os.path.exists(self.student_folder_path):
+            os.makedirs(self.student_folder_path)
+
+        self.image_label.pack(padx=6, pady=6, fill="both")
+        if self.cap is not None:
+            self.cap.release()
+        self.cap = cv2.VideoCapture(self.get_camera_choice())
+        self.running = True
+        self.update_frame()
+        return
+
+    def save_face(self):
+        self.start = True
+
+    def process_frame(self, frame: cv2.typing.MatLike):
+        if self.start:
+            face_locations = face_recognition.face_locations(frame)
+            for face_location in face_locations:
+                if len(face_locations) == 1:
+                    self.frame_count += 1
+                    top, right, bottom, left = face_locations[0]
+                    # Save the face
+                    margin = 50  # You can adjust this value
+                    top1 = max(top - margin, 0)
+                    right1 = min(right + margin, frame.shape[1])
+                    bottom1 = min(bottom + margin, frame.shape[0])
+                    left1 = max(left - margin, 0)
+                    face = frame[top1:bottom1, left1:right1]
+                    file_name_path = (
+                        f"{self.student_folder_path}/{str(self.frame_count)}.jpg"
+                    )
+                    cv2.imwrite(file_name_path, face)
+                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+                    cv2.putText(
+                        frame,
+                        str(self.frame_count),
+                        (100, 200),
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        2,
+                        (0, 255, 0),
+                        2,
+                    )
+                elif len(face_locations) > 1:
+                    top, right, bottom, left = face_location
+                    cv2.rectangle(frame, (left, top), (right, bottom), (255, 0, 0), 2)
+        if self.frame_count >= 100:
+            self.stop_camera()
+
+    def update_frame(self):
+        if not self.running:
+            return  # Stop updating if the camera has been stopped
+
+        ret, frame = self.cap.read()
+        if not ret:
+            return  # If frame is not read correctly
+        self.process_frame(frame)
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        img = Image.fromarray(cv2image)
+        imgtk = CTkImage(
+            img, size=(self.image_label.winfo_width(), self.image_label.winfo_height())
+        )
+        self.image_label.configure(image=imgtk)
+        self.after(10, self.update_frame)
+
+    def stop_camera(self):
+        if self.cap is not None:
+            self.cap.release()
+            self.cap = None
+        self.running = False
+        self.image_label.configure(image=None)
+        self.image_label.pack_forget()
+        self.parent.submit_button.configure(state="normal")
+        self.start = False
+        self.frame_count = 0
+
+    def get_camera_choice(self):
+        with open("choice.json", "r") as f:
+            return int(json.load(f)[0])
+
+
+class StudentTableFrame(CTkFrame):
     def __init__(self, parent, db: Database, **kwargs):
         super().__init__(parent, **kwargs)
         self.db = db
-        self.configure(border_width=4, border_color="#5665EF")
+        self.configure(border_width=4, border_color="#5665EF", corner_radius=0)
         self.create_widgets()
         self.show_data()
 
@@ -279,12 +447,9 @@ class ShowDataFrame(CTkFrame):
         self.filter_logo = CTkImage(Image.open("src/filter.png"), size=(25, 25))
 
         self.filter_frame = CTkFrame(self, fg_color="#B188A8", corner_radius=3)
-        self.filter_frame.pack(side="top", fill="x", padx=6, pady=(6,1))
-        
-        
-        
-        self.table_frame = CTkScrollableFrame(self, corner_radius=3)
-        self.table_frame.pack(fill="both",expand=True,padx=6,pady=(1,6))
+        self.filter_frame.pack(side="top", fill="x", padx=6, pady=(6, 1))
+
+        self.create_table()
 
         # Course filter widgets
         self.course_frame = CTkFrame(self.filter_frame, bg_color="#B188A8")
@@ -297,6 +462,7 @@ class ShowDataFrame(CTkFrame):
 
         self.filter_course_entry = CTkComboBox(
             self.course_frame,
+            command=self.show_data,
             values=["ALL", "BSC", "BCA", "BBA"],
             bg_color="#B188A8",
             state="readonly",
@@ -315,6 +481,7 @@ class ShowDataFrame(CTkFrame):
 
         self.filter_sem_entry = CTkComboBox(
             self.sem_frame,
+            command=self.show_data,
             values=["ALL", "1st", "2nd", "3rd", "4th", "5th", "6th"],
             bg_color="#B188A8",
             state="readonly",
@@ -322,18 +489,46 @@ class ShowDataFrame(CTkFrame):
         self.filter_sem_entry.set("ALL")
         self.filter_sem_entry.pack(side="left")
 
-        self.filter_button = CTkButton(
-            self.filter_frame,
-            text="",
-            image=self.filter_logo,
-            command=self.show_data,
-            width=20,
-            height=20,
-            fg_color="#B188A8",
-            bg_color="#B188A8",
-            hover_color="#886981",
+    def create_table(self):
+        style = ttk.Style()
+        style.theme_use("clam")
+
+        # Configure Treeview colors
+        style.configure(
+            "Treeview",
+            background="#363636",
+            fieldbackground="#363636",
+            foreground="white",
+            font=("Arial", 10),
         )
-        self.filter_button.pack(side="left", padx=5, pady=5)
+
+        # Change selected color
+        style.map("Treeview", background=[("selected", "#4a4a4a")])
+
+        # Scrollbar for the Treeview
+        tree_scroll = tk.Scrollbar(self, width=15)
+        tree_scroll.pack(side=tk.RIGHT, fill=tk.Y, padx=(0, 8), pady=(1, 8))
+
+        # Create the Treeview
+        self.tree = ttk.Treeview(
+            self, yscrollcommand=tree_scroll.set, selectmode="extended"
+        )
+        self.tree.pack(side="left", fill="both", expand=True, padx=(8, 0), pady=(1, 8))
+        # Configure the Scrollbar
+        tree_scroll.config(command=self.tree.yview)
+
+        # Define columns
+        self.tree["columns"] = ("Roll", "Name", "Course", "Sem")
+        self.tree.column("#0", width=0, stretch=tk.NO)
+        self.tree.column("Roll", anchor=tk.W, width=120)
+        self.tree.column("Name", anchor=tk.W, width=120)
+        self.tree.column("Course", anchor=tk.W, width=120)
+        self.tree.column("Sem", anchor=tk.W, width=120)
+        # Define headings
+        self.tree.heading("Roll", text="Roll", anchor=tk.W)
+        self.tree.heading("Name", text="Name", anchor=tk.W)
+        self.tree.heading("Course", text="Course", anchor=tk.W)
+        self.tree.heading("Sem", text="Sem", anchor=tk.W)
 
     def get_filter_data(self):
         course = self.filter_course_entry.get()
@@ -354,28 +549,31 @@ class ShowDataFrame(CTkFrame):
                 params.append(sem)
 
             query = base_query + " WHERE " + " AND ".join(conditions)
+        query += " ORDER BY roll"
         return self.db.fetch_data(query, params)
-    
-    
-    def show_data(self):
-        data = self.get_filter_data()
-        for row in data:
-            frame = CTkFrame(self.table_frame,height=20,fg_color="#363636")
-            frame.pack(fill='x', padx=10, pady=1)
 
-            for col_idx, item in enumerate(row):
-                label = CTkLabel(master=frame, text=item,height=15,corner_radius=5, anchor='w')
-                label.place(relx = self.xr[col_idx], rely = 0.49,anchor="center",relwidth = self.wr[col_idx])
-                
+    def clear_tree(self):
+        for item in self.tree.get_children():
+            self.tree.delete(item)
+
+    def show_data(self, *args):
+        data = self.get_filter_data()
+        self.clear_tree()
+        for row in data:
+            self.tree.insert("", "end", values=row)
+
+
 class TrainDataFrame(CTkFrame):
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
         # Initialize your train data frame widgets here
 
+
 class AttendanceFrame(CTkFrame):
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
         # Initialize your attendance frame widgets here
+
 
 if __name__ == "__main__":
     app = MyApp()
